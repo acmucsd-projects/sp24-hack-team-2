7b51325091c201333cd29b2ea6300b8d4d24323e
jtran-9 <jrtran@ucsd.edu>
2024-05-25 17:52:22 -0700

    update and delete trips from user schemas

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const User = require('../database_schema/userSchema');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -8,6 +9,45 @@ router.get('/', function(req, res, next) {
     email: 'hack@acmucsd.org'
   }
   res.status(200).json({ user });
+});
+
+router.delete('/delete-trip', async (req, res) => {
+  const {userID, trip} = req.body
+
+  try {
+    const user = await User.findOne( { firebaseUID: userID } )
+
+    if(!user) {
+      res.status(404).send('User not found')
+    }
+
+    user.trips = user.trips.filter(entry => entry.tripID !== trip);
+
+    await user.save()
+
+    res.status(200).send(user)
+  } catch (err) {
+    res.status(500).send(err.message)
+  }
+});
+
+router.post('/add-trip', async (req, res) => {
+  const { userID, trip } = req.body
+
+  try {
+    const user = await User.findOne( { firebaseUID: userID } )
+
+    if(!user) {
+      res.status(404).send('User not found')
+    }
+
+    user.trips = user.trips.push(trip)
+    await user.save()
+
+    res.status(200).send(user)
+  } catch (err) {
+    res.status(500).send(err.message)
+  }
 });
 
 module.exports = router;
