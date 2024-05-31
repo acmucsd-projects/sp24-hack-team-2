361b159e1ba8e906fc0f8d05012ebc89fc6fff6d
deeg-p23 <23diegopereyra@gmail.com>
2024-05-31 13:43:41 -0700

    finished login modal, added /register and /account-recovery pages
    next/navigation routes added as well, logins and registrations redirect user to the /start page to begin itinerary.
    
    maybe as a stretch goal after the login endpoint is complete, we can consider adding the google authentication api?

---
+++ client/src/app/register/page.js
@@ -0,0 +1,147 @@
+'use client';
+
+import styles from './../page.module.css';
+
+import Image from 'next/image';
+import { useRouter } from 'next/navigation';
+
+import { useEffect } from 'react';
+import { useState } from 'react';
+
+import { motion as m } from 'framer-motion';
+
+
+// This will be our page for the Register page.
+
+export default function Register() {
+    const [useEmail, setEmail] = useState("");
+    const changeEmail = event => {
+        setEmail(event.target.value)
+    }
+
+    const [usePass, setPass] = useState("");
+    const changePass = event => {
+        setPass(event.target.value)
+    }
+
+    const clickLogin = () => {
+      console.log("Email: ", useEmail, " Password: ", usePass);
+      
+      if ((useEmail.length > 0) && (usePass.length > 0)) {
+        routeToStart();
+      }
+    }
+
+    const router = useRouter();
+    function routeToStart() {
+      router.replace("/start");
+    }
+
+    // useEffect to remedy toggleCheckbox bug
+    // { checking on an empty password field will invert visibility }
+    useEffect(() => {
+      if (usePass.length == 0) {
+        document.getElementById("passwordHideImg").src="/password_blank.png";
+      }
+      else {
+        var x = document.getElementById("passwordBox");
+        if (x.type == "password") {
+          document.getElementById("passwordHideImg").src="/password_eyeclosed.png"
+        }
+        else if (x.type == "text") {
+          document.getElementById("passwordHideImg").src="/password_eyeopen.png"
+        }
+      }
+    }, [usePass])
+
+    function toggleCheckbox() {
+      var x = document.getElementById("passwordBox");
+      console.log("clicked");
+      document.getElementById("passwordHideImg").src="/password_eyeclosed.png";
+      if (x.type == "password") {
+        x.type = "text";
+        document.getElementById("passwordHideImg").src="/password_eyeopen.png";
+      }
+      else {
+        x.type = "password";
+        document.getElementById("passwordHideImg").src="/password_eyeclosed.png";
+      }
+      console.log(document.getElementById("passwordHideImg").src);
+    }
+
+    return (
+      <>
+        <m.div className={styles.registerPanel}
+          initial={{opacity: 0}}
+          animate={{opacity: 1}}
+          transition={{ duration: 0.75, ease: 'easeOut' }}
+          exit={{ opacity: 1 }}
+        >
+          <m.div className={styles.registerContainer}
+            initial={{y: 145}}
+            animate={{y: 95}}
+            transition={{ duration: 0.75, ease: 'easeOut' }}
+            exit={{ opacity: 1 }}          
+          >
+            <br/>
+            <Image
+              src="/geogurulogo.svg"
+              alt="Geoguru Logo"
+              className={styles.logoimg}
+              width={38}
+              height={54}
+              priority
+            />
+            <div className={styles.registerOvertext}>
+              <h3>Create your account</h3>
+              <p>Sign up with an email and password below to get started with Geoguru!</p>
+              <br/>
+            </div>
+            <div className={styles.logininfo}>
+              <h3>
+                Email Address
+              </h3>
+              <div className={styles.logininfoboxes}>
+                <div className={styles.passwordbox}>
+                  <div className={styles.passwordinput}>
+                    <input onChange={changeEmail}
+                    value = {useEmail}>
+                    </input>
+                  </div>
+                </div>
+              </div>
+              <h2></h2>
+              <h3>
+                Password
+              </h3>
+              <div className={styles.logininfoboxes}>
+                <div className={styles.passwordbox}>
+                  <div className={styles.passwordinput}>
+                    <input onChange={changePass}
+                    value={usePass} type="password" id="passwordBox">
+                    </input>
+                    <div className={styles.passwordbutton}>
+                      <button onClick={toggleCheckbox}>
+                        <img
+                          src="/password_blank.png"
+                          alt="Geoguru Logo"
+                          id="passwordHideImg"
+                          className={styles.passwordhide}
+                          width={25}
+                          height={25}
+                        />
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <br/>
+            </div>
+            <div className={styles.signupButton}>
+              <button onClick={clickLogin}>Sign up</button>
+            </div>
+          </m.div>
+        </m.div>
+      </>
+    );
+}
